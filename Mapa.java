65511997285a89da9cb540e3ba11eb21eb0c436c
Tomasz Kociński <kociak0@gmail.com>
2015-04-08 23:39:17 +0200

    5 pkt

--- Mapa.java
+++ Mapa.java
@@ -4,7 +4,9 @@ package pl.edu.uksw.wmp.prja.laboratorium5;
 public class Mapa {
     MapOfLinkedCities[] MapOfLinCit;
     int wherePutNewCity;
-
+    Miasto[][] allConnections=new Miasto[255][255];
+    Integer column=0,row=0;
+        
     public Mapa() {
         MapOfLinCit = new MapOfLinkedCities[255];
         wherePutNewCity = 0;
@@ -41,26 +43,26 @@ public class Mapa {
         if (skad.toString().equals(dokad.toString())) {
             return true;
         }
-        MapOfLinkedCities RootCity = null;
+        MapOfLinkedCities tempRootCity = null;
         for (int i = 0; i < wherePutNewCity; i++) {
             if (skad.toString().equals(MapOfLinCit[i].RootCity.toString())) {
-                RootCity = MapOfLinCit[i];
+                tempRootCity = MapOfLinCit[i];
                 break;
             }
         }
-        if (RootCity == null) {
+        if (tempRootCity == null) {
             throw new MiastoNieIstniejeException();
         }
         MapOfLinkedCities DestinationCity = null;
-        for (int i = 0; i < RootCity.howMuchConnectedCity; i++) {
-            if (dokad.toString().equals(RootCity.DCaD[i].B.toString())) {
+        for (int i = 0; i < tempRootCity.howMuchConnectedCity; i++) {
+            if (dokad.toString().equals(tempRootCity.DCaD[i].B.toString())) {
                 DestinationCity = MapOfLinCit[i];
                 break;
             }
         }
         if (DestinationCity == null) {
-            for (int i = 0; i < RootCity.howMuchConnectedCity; i++) {
-                return czyIstniejeDroga(RootCity.DCaD[i].B, dokad);
+            for (int i = 0; i < tempRootCity.howMuchConnectedCity; i++) {
+                return czyIstniejeDroga(tempRootCity.DCaD[i].B, dokad);
             }
         }
         return DestinationCity
@@ -96,13 +98,63 @@ public class Mapa {
     }
 
     public Miasto[] podajNajkrotszaDroge(Miasto skad, Miasto dokad) throws MiastoNieIstniejeException, DrogaNieIstniejeException {
+        zaglebienie(skad, dokad);
+       // Miasto[][] allConSkrocne;
+        Miasto[] najmniejszy=null;
+        int dlugosc=9999999;
+        for (int i = 0; allConnections[i]!=null; i++) {
+            
+            int j;
+            for ( j = 0; allConnections[i][j]!=null; j++) {
+            }
+            Miasto[] terazniejszy=new Miasto[j];
+            System.arraycopy(allConnections[i], 0, terazniejszy, 0, j);
+            if(terazniejszy.length<2)break;
+            int int_temp=podajDlugoscDrogi(terazniejszy);
+            if(dlugosc>int_temp){
+                najmniejszy=terazniejszy;
+                dlugosc=int_temp;
+            }
+            
+            
+        }
+        
+        
+        return najmniejszy;
+    }
+    public boolean zaglebienie(Miasto skad, Miasto dokad) throws MiastoNieIstniejeException, DrogaNieIstniejeException {
+        System.out.println(column+" "+row);   
         if(!czyIstniejeDroga(skad, dokad)){
             throw new DrogaNieIstniejeException();
         }
+        MapOfLinkedCities from=null,where=null;
+        for (int i = 0; i < wherePutNewCity; i++) {
+            if (skad.toString().equals(MapOfLinCit[i].RootCity.toString())) {
+                from = MapOfLinCit[i];
+            }
+            if (dokad.toString().equals(MapOfLinCit[i].RootCity.toString())) {
+                where = MapOfLinCit[i];
+            }
+            if (from != null && where !=null) break;
+        }
+        if (from == null || where==null) {
+            throw new MiastoNieIstniejeException();
+        }
+        if(from.toString().equals(where.toString()))
+        {
+            System.arraycopy(allConnections[column], 0, allConnections[column+1], 0, row-1);
+            allConnections[column++][row]=from.RootCity;
+            return true;
+        }
+        for (int i = 0; i < from.howMuchConnectedCity; i++) {
+            allConnections[column][row++]=from.RootCity;
+            zaglebienie(from.DCaD[i].B, dokad);
+                row--;
+        }
         
-        return new Miasto[]{};
+        return false;
+     
     }
-    
     public void DoesCitiesExistInMap(Miasto from, Miasto where) throws MiastoNieIstniejeException {
         boolean FirstIsInMap = false, SecoundIsInMap = false;
         for (int i = 0; i < wherePutNewCity; i++) {
